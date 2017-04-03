18f0c34db4bb9c6b94f7f73d1a882ed02c7974da
lisaztruong <lisaztruong@gmail.com>
2017-04-03 13:52:05 -0700

    moved files from propertyfinder app

--- adoeapp/index.ios.js
+++ adoeapp/index.ios.js
@@ -4,41 +4,42 @@
  * @flow
  */
 
+'use strict'
+
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View,
-  StackNavigator,
+  NavigatorIOS
 } from 'react-native';
 
-export default class adoeapp extends Component {
+var SearchPage = require('./SearchPage');
+
+class adoeapp extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to ADOE
-        </Text>
-        <Text style={styles.instructions}>
-          Donate to social good
-        </Text>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Charity Finder',
+          component: SearchPage,
+        }}/>
     );
   }
 }
 
-const App = StackNavigator({
-  Main: {screen: MainScreen},
-  Profile: {screen: ProfileScreen}
-})
-
 const styles = StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+
   },
   welcome: {
     fontSize: 20,
