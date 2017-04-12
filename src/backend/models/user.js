16b71cf854149ef42560c1299050080098fe2b10
lisaztruong <lisaztruong@gmail.com>
2017-04-12 14:05:32 -0700

    adding validations

--- src/backend/models/user.js
+++ src/backend/models/user.js
@@ -14,7 +14,11 @@ var userSchema  = mongoose.Schema({
   email: {
     type: String,
     unique: true,
-    required: true
+    required: true,
+    //  HTML5 email validation regex
+    validate: function(email) {
+      return /^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)
+    }
   },
   password: {
     type: String,
@@ -22,7 +26,16 @@ var userSchema  = mongoose.Schema({
   },
   phoneNumber: {
     type: String,
-    unique: true
+    unique: true,
+    validate: {
+      validator: function(num) {
+        // 10 digits
+        var regexStuff = /^\d{10}$/;
+        // checks that number exists and then checks against regex
+        return (num === null || num.trim().length < 1) || regexStuff.test(num)
+      },
+      message: 'Provided phone number is invalid and/or not a US number'
+    }
   },
   streetAddress: {
     type: String
