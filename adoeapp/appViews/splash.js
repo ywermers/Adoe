c70b41a46d49e371f940b388f62b2b70d3be8178
Yasmeen Wermers <yasmeen.wermers@emory.edu>
2017-04-14 13:05:57 -0700

    updated splash

--- adoeapp/appViews/splash.js
+++ adoeapp/appViews/splash.js
@@ -1,40 +1,52 @@
 import React, { Component } from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
 export default class splash extends Component {
   render() {
     return (
-      <View style={styles.wrapper}>
-        <View style={styles.titleWrapper}>
+      <View style={styles.container}>
+        <View style={styles.logoContainer}>
+          <Image
+          style={styles.logo}
+          source={require('../assets/heart.png')}
+          />
           <Text style={styles.title}>Adoe</Text>
         </View>
-        <View>
-        <Text style={styles.subtitle}>Powered By Yasmeen</Text>
       </View>
-    </View>
+
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container:{
+    backgroundColor: "#058ed9",
+    flex: 1,
+    justifyContent: 'center'
+  },
   wrapper: {
     backgroundColor: '#058ed9',
-    flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
   },
+  logo:{
+    width:100,
+    height: 100,
+    justifyContent: 'center'
+  },
+  logoContainer:{
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
   title: {
-    color: 'white',
+    color: '#f4ebd9',
     fontSize: 35,
     fontWeight: 'bold'
   },
   subtitle: {
-    color: 'white',
+    color: '#f4ebd9',
     fontWeight: '200',
     paddingBottom: 50
   },
-  titleWrapper: {
-    flex: 1,
-    justifyContent: 'center'
-  }
+
 });
